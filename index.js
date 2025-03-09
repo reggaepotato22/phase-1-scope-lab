a82a8cd12fe6e5edce90ef6dbc0824a9fe7b8fe2
Andrew <andrewmandieka@gmail.com>
2025-03-09 21:41:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declared globally (without var, let, or const)
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  const leastFavoriteCustomer = 'someone';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'anyone'; // This will cause an error
+  }
+        
